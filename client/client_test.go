b7bd8c999bdf711652abcfa0e926ca941904d357
bruteforce1414 <bruteforce1414@gmail.com>
2018-12-05 14:04:46 +0500

    Парсинг страниц реализован

--- client/client_test.go
+++ client/client_test.go
@@ -5,11 +5,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/bruteforce1414/simple-web-client-with-test/client"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"net/http"
 	"net/http/httptest"
-	"strings"
+	"net/url"
 	"strconv"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestHttpClient_Get(t *testing.T) {
@@ -38,7 +41,7 @@ func TestHttpClient_Get(t *testing.T) {
 	}
 	fmt.Println("Ответ сервера: ", string(resp))
 	for i, link := range FindURLs(string(resp)) {
-		fmt.Println("Ссылка №", i+1, ": ", link)
+		fmt.Println("Ссылка №",i+1,": ", link)
 	}
 	a.Equal(tURLS, FindURLs(string(resp)))
 }
@@ -56,4 +59,61 @@ func FindURLs(body string) []string {
 
 	})
 	return returnedLinks
+}
+
+
+
+func TestHttpClient_Get2(t *testing.T) {
+	a := assert.New(t)
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	tURLS := []string{
+		server.URL ,"/teachers/283208/","info",
+	}
+	mux.HandleFunc("/teachers/283208/", func(w http.ResponseWriter, r *http.Request) {
+		var links string
+		var s string
+		var link string
+		for _, link = range tURLS {
+			links = links + "<a href=" + strconv.Quote(link) + ">" + link + "</a>"
+		}
+		s = fmt.Sprintf("<html><body>%s</body></html>", links)
+		w.Write([]byte(s))
+	})
+
+	mux.HandleFunc("/teachers/283208/info", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello!!!"))
+	})
+
+	reqGetText:="/teachers/283208/"
+	clientTest := client.NewHttpClient(server.URL)
+	resp, err := clientTest.Get(reqGetText)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ответ сервера: ", string(resp))
+	for i, link := range FindURLs(string(resp)) {
+		fmt.Println("Ссылка №",i+1,": ", link)
+		link=ParseUrl(server.URL+reqGetText,link)
+		fmt.Println("Полный путь по ссылке №",i+1,": ", link)
+	}
+	a.Equal(tURLS, FindURLs(string(resp)))
+	time.Sleep(100000)
+
+}
+
+func ParseUrl(urlPage string, ctxUrl string) string{
+var fullLink string
+	u, err := url.Parse(ctxUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	base, err := url.Parse(urlPage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullLink=(base.ResolveReference(u)).String()
+return fullLink
 }

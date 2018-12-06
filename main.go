af87e75efe2a30c20d9425ab202a9b86f456f903
bruteforce1414 <bruteforce1414@gmail.com>
2018-12-06 14:27:19 +0500

    Находится 43 ссылки, парсится 121

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"github.com/bruteforce1414/crawler/client"
 	"github.com/streadway/amqp"
+	"io/ioutil"
 	"log"
-	"time"
+	"net/http"
 )
 
 func failOnError(err error, msg string) {
@@ -15,8 +16,9 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-
+	Publicher()
 	go worker()
+	fmt.Scanln()
 }
 
 func worker() {
@@ -59,16 +61,70 @@ func worker() {
 	forever := make(chan bool)
 
 	go func() {
+
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
-			dot_count := bytes.Count(d.Body, []byte("."))
-			t := time.Duration(dot_count)
-			time.Sleep(t * time.Second)
+			resp, err := http.Get(string(d.Body))
+			if err != nil {
+				panic(err)
+			}
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+			bodyString := string(bodyBytes)
+			//	var linksHttp []string
+			linksAll := urlprocessing.FindURLs(bodyString)
+			for i, link := range linksAll {
+
+				fmt.Println("Cсылка №", i+1, ": ", link)
+				link = urlprocessing.ParseUrl(string(d.Body), link)
+				if link != "" {
+					fmt.Println("Полный путь по ссылке №", i+1, ": ", link)
+				}
+				if link == "" {
+					fmt.Println("Ссылка №", i+1, "не относится к формату text/html", link)
+				}
+			}
 			log.Printf("Done")
 			d.Ack(false)
+
 		}
+
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
+}
+
+func Publicher() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"task_queue", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "http://www.old.fa.ru/fil/ufa/Pages/cont.aspx"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
 }
